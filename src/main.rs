bef262ad4bb767259af3313ea1e59a3f288c1f01
zimrustpro <zimrustpro@gmail.com>
2024-11-21 09:53:51 +0200

    Bit more complex trait example

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
-use std::fmt::Display;
-
-struct Cat {
-    name: String,
-    age: u8,
+struct Monster {
+    health: usize,
 }
+
+struct Wizard {}
+struct Ranger {}
+
+trait FightClose {
+    fn attack_with_sword(&self, opponent: &mut Monster) {
+        opponent.health -= 10;
+        println!(
+            "Sword attack! Your opponent has {} health left.",
+            opponent.health
+        );
+    }
+
+    fn attack_with_hand(&self, opponent: &mut Monster) {
+        opponent.health -= 2;
+        println!(
+            "Hand attack! Your opponent has {} health left.",
+            opponent.health
+        );
+    }
+}
+
+trait FightFromDistance {
+    fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 10 {
+            opponent.health -= 10;
+            println!(
+                "Bow attack! Your opponent has {} health left.",
+                opponent.health
+            );
+        }
+    }
+
+    fn attack_with_rock(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 3 {
+            opponent.health -= 4;
+            println!(
+                "Rock attack! Your opponent has {} health left.",
+                opponent.health
+            );
+        }
+    }
+}
+
+impl FightClose for Wizard {}
+impl FightClose for Ranger {}
+impl FightFromDistance for Ranger {}
 
 fn main() {
-    let mr_mantle = Cat {
-        name: "Reggie Mantle".to_string(),
-        age: 4,
-    };
-    println!("{mr_mantle}");
-    print_excitedly(mr_mantle.to_string());
-    println!(
-        "Mr. Mantle's String is {} characters long.",
-        mr_mantle.to_string().chars().count()
-    );
-}
-
-fn print_excitedly(input: String) {
-    println!("{input}!!!!!");
-}
-
-impl Display for Cat {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} is a cat who is {} years old", self.name, self.age)
-    }
+    let radagst = Wizard {};
+    let aragon = Ranger {};
+    let mut uruk_hai = Monster { health: 40 };
+    radagst.attack_with_sword(&mut uruk_hai);
+    aragon.attack_with_bow(&mut uruk_hai, 8);
 }
