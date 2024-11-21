6c6d44639abe562f8b2c581744f0635ec00f939d
zimrustpro <zimrustpro@gmail.com>
2024-11-21 09:23:39 +0200

    make a dog-like trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let rover = Dog {
+        name: "Rover".to_string(),
+    };
+    let brian = Parrot {
+        name: "Brian".to_string(),
+    };
+    rover.bark();
+    rover.run();
+    brian.bark();
 }
+
+struct Dog {
+    name: String,
+}
+
+struct Parrot {
+    name: String,
+}
+
+trait DogLike {
+    fn bark(&self) {
+        println!("Woof woof!");
+    }
+
+    fn run(&self) {
+        println!("The dog is running!")
+    }
+}
+
+impl DogLike for Dog {}
+impl DogLike for Parrot {}
