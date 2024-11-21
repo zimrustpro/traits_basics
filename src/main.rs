b7e121f8caad21be06dd4291affa45b5287f34dd
zimrustpro <zimrustpro@gmail.com>
2024-11-21 10:31:39 +0200

    Traits as bounds (traits without methods inside)

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,73 @@
 use std::fmt::Debug;
 
-#[derive(Debug)]
 struct Monster {
     health: i32,
 }
 
-trait MonsterBehaviour: Debug {
-    fn take_damage(&mut self, damage: i32);
-
-    fn display_self(&self) {
-        println!("The monster is now: {self:?}");
-    }
-}
-
-impl MonsterBehaviour for Monster {
-    fn take_damage(&mut self, damage: i32) {
-        self.health -= damage;
-    }
-}
-
 #[derive(Debug)]
 struct Wizard {
-    health: i32,
+    _health: i32,
 }
 
 #[derive(Debug)]
 struct Ranger {
-    health: i32,
+    _health: i32,
 }
 
-trait FightClose: Debug {
-    fn attack_with_sword<T: MonsterBehaviour>(&self, opponent: &mut T) {
-        println!("You attack with your sword!");
-        opponent.take_damage(10);
-        opponent.display_self();
-    }
+trait Magic {}
+trait FightClose {}
+trait FightFromDistance {}
 
-    fn attack_with_hand<T: MonsterBehaviour>(&self, opponent: &mut T) {
-        println!("You attack with your hand!");
-        opponent.take_damage(2);
-        opponent.display_self();
-    }
-}
-
-impl FightClose for Wizard {}
 impl FightClose for Ranger {}
-
-trait FightFromDistance: Debug {
-    fn attack_with_bow<T: MonsterBehaviour>(&self, opponent: &mut T, distance: u32) {
-        println!("You attack with your bow!");
-        if distance < 10 {
-            opponent.take_damage(10);
-        } else {
-            println!("Too far away!");
-        }
-        opponent.display_self();
-    }
-
-    fn attack_with_rock<T: MonsterBehaviour>(&self, opponent: &mut T, distance: u32) {
-        println!("You attack with your rock!");
-        if distance < 3 {
-            opponent.take_damage(4);
-        } else {
-            println!("Too far away!")
-        }
-        opponent.display_self();
-    }
-}
-
 impl FightFromDistance for Ranger {}
 
+impl Magic for Wizard {}
+impl FightClose for Wizard {}
+
+fn attack_with_bow<T>(pc: &T, opponent: &mut Monster, distance: u32)
+where
+    T: FightFromDistance + Debug,
+{
+    if distance < 10 {
+        opponent.health -= 10;
+        println!(
+            "Bow attack! Opponent's health: {}. You are now at {pc:?}",
+            opponent.health
+        );
+    }
+}
+
+fn attack_with_sword<T>(pc: &T, opponent: &mut Monster)
+where
+    T: FightClose + Debug,
+{
+    opponent.health -= 10;
+    println!(
+        "Sword attack! Opponent's health: {}. You are now at {pc:?}",
+        opponent.health
+    );
+}
+
+fn fireball<T>(pc: &T, opponent: &mut Monster, distance: u32)
+where
+    T: Magic + Debug,
+{
+    if distance < 15 {
+        opponent.health -= 20;
+        println!(
+            "A massive fireball! Opponent's health: {}. You are now at {pc:?}",
+            opponent.health
+        );
+    }
+}
+
 fn main() {
-    let radagast = Wizard { health: 60 };
-    let aragorn = Ranger { health: 80 };
+    let radagast = Wizard { _health: 60 };
+    let aragorn = Ranger { _health: 80 };
 
     let mut uruk_hai = Monster { health: 40 };
 
-    radagast.attack_with_sword(&mut uruk_hai);
-    aragorn.attack_with_bow(&mut uruk_hai, 8);
+    attack_with_sword(&radagast, &mut uruk_hai);
+    attack_with_bow(&aragorn, &mut uruk_hai, 8);
+    fireball(&radagast, &mut uruk_hai, 8);
 }
