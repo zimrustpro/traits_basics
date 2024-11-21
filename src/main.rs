f32851c5193ed7353f4820c1627a15645c4a258a
zimrustpro <zimrustpro@gmail.com>
2024-11-21 10:17:30 +0200

    Enhance trait by making it take types that implement other traits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::fmt::Debug;
 
+#[derive(Debug)]
 struct Monster {
     health: i32,
+}
+
+trait MonsterBehaviour: Debug {
+    fn take_damage(&mut self, damage: i32);
+
+    fn display_self(&self) {
+        println!("The monster is now: {self:?}");
+    }
+}
+
+impl MonsterBehaviour for Monster {
+    fn take_damage(&mut self, damage: i32) {
+        self.health -= damage;
+    }
 }
 
 #[derive(Debug)]
@@ -15,20 +30,16 @@ struct Ranger {
 }
 
 trait FightClose: Debug {
-    fn attack_with_sword(&self, opponent: &mut Monster) {
-        opponent.health -= 10;
-        println!(
-            "Sword attack! Your opponent has {} health left. You are now at: {:?}",
-            opponent.health, self
-        );
+    fn attack_with_sword<T: MonsterBehaviour>(&self, opponent: &mut T) {
+        println!("You attack with your sword!");
+        opponent.take_damage(10);
+        opponent.display_self();
     }
 
-    fn attack_with_hand(&self, opponent: &mut Monster) {
-        opponent.health -= 2;
-        println!(
-            "Hand attack! Your opponent has {} health left. You are now at: {:?}",
-            opponent.health, self
-        );
+    fn attack_with_hand<T: MonsterBehaviour>(&self, opponent: &mut T) {
+        println!("You attack with your hand!");
+        opponent.take_damage(2);
+        opponent.display_self();
     }
 }
 
@@ -36,24 +47,24 @@ impl FightClose for Wizard {}
 impl FightClose for Ranger {}
 
 trait FightFromDistance: Debug {
-    fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
+    fn attack_with_bow<T: MonsterBehaviour>(&self, opponent: &mut T, distance: u32) {
+        println!("You attack with your bow!");
         if distance < 10 {
-            opponent.health -= 10;
-            println!(
-                "Bow attack! Your opponent has {} health left. You are now at {self:?}",
-                opponent.health
-            );
+            opponent.take_damage(10);
+        } else {
+            println!("Too far away!");
         }
+        opponent.display_self();
     }
 
-    fn attack_with_rock(&self, opponent: &mut Monster, distance: u32) {
+    fn attack_with_rock<T: MonsterBehaviour>(&self, opponent: &mut T, distance: u32) {
+        println!("You attack with your rock!");
         if distance < 3 {
-            opponent.health -= 4;
-            println!(
-                "Rock attack! Your opponent has {} health left. You are now at {self:?}",
-                opponent.health
-            );
+            opponent.take_damage(4);
+        } else {
+            println!("Too far away!")
         }
+        opponent.display_self();
     }
 }
 
@@ -62,7 +73,9 @@ impl FightFromDistance for Ranger {}
 fn main() {
     let radagast = Wizard { health: 60 };
     let aragorn = Ranger { health: 80 };
+
     let mut uruk_hai = Monster { health: 40 };
+
     radagast.attack_with_sword(&mut uruk_hai);
     aragorn.attack_with_bow(&mut uruk_hai, 8);
 }
